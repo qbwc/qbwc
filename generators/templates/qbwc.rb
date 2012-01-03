59bffed3ba10c9553eca9d999e4027818505314f
Russell Osborne <russell@burningpony.com>
2012-01-02 23:54:32 -0500

    Adding User/Pass to Config Options

--- generators/templates/qbwc.rb
+++ generators/templates/qbwc.rb
@@ -1,5 +1,9 @@
 QBWC.setup do |config|
   
+  #Currently Only supported for single logins. 
+  config.qbwc_username = "foo"
+  config.qbwc_password = "bar"
+  
   #Minimum Quickbooks Version Required for use in QBXML Requests
   config.quickbooks_min_version = 3.0
   
@@ -7,7 +11,7 @@ QBWC.setup do |config|
   config.quickbooks_type = :qb
   
   #Quickbooks Support URL provided in QWC File
-  config.quickbooks_support_site_url = "http://qb_support.lumber.com"
+  config.quickbooks_support_site_url = "localhost:3000"
   
   #Quickbooks Owner ID provided in QWC File
   config.quickbooks_owner_id = '{57F3B9B1-86F1-4fcc-B1EE-566DE1813D20}'
