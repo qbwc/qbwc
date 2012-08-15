cb502733426ee4d12bf0acc7d894ab4f6ef99b7b
germanc4 <german@3drobotics.com>
2012-08-14 17:09:35 -0700

    Fixing AuthenticateResponse namespace

--- lib/qbwc/soap_wrapper/defaultServant.rb
+++ lib/qbwc/soap_wrapper/defaultServant.rb
@@ -39,7 +39,7 @@ class QBWC::QBWebConnectorSvcSoap
   #
   def authenticate(parameters)
     #p parameters                               
-    AuthenticateResponse.new(['foo', QBWC.quickbooks_company_file_path]) #path to company file
+    QBWC::AuthenticateResponse.new([QBWC.username, QBWC.company_file_path]) #path to company file
   end
 
   # SYNOPSIS
