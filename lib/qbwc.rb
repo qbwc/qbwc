796a41992ec747dba1dc424bf22dc5d4e7f4746b
christian <okaychemicals@gmail.com>
2013-01-21 11:45:53 -0800

    Forgot to push it with the .reverse
    Signed-off-by: christian <okaychemicals@gmail.com>

--- lib/qbwc.rb
+++ lib/qbwc.rb
@@ -50,7 +50,7 @@ class << self
 
   def add_job(name,priority=0, &block)
     @@jobs[name] = Job.new(name,priority, &block)
-    @@jobs.sort_by{|job, priority| job[3] }
+    @@jobs.sort_by{|job, priority| job[3] }.reverse
   end
   
   def on_error=(reaction)
