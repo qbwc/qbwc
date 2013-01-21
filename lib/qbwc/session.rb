b57283a24f8e27d63f3cc2ea36e3f231551a5401
christian <okaychemicals@gmail.com>
2013-01-21 11:29:11 -0800

    I think this sort should do the trick.
    Signed-off-by: christian <okaychemicals@gmail.com>

--- lib/qbwc/session.rb
+++ lib/qbwc/session.rb
@@ -62,6 +62,7 @@ class QBWC::Session
 
   def build_request_generator(jobs)
     Fiber.new do
+      jobs.sort_by{|job, priority| job[3] }.reverse
       jobs.each do |j|
         @current_job = j
         while (r = next_request)
