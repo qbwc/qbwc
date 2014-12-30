23af3e7f42cd89604fa9fe9863f733830535d9a1
Rob Chekaluk <rchekaluk@ppolitics.org>
2014-12-30 18:27:44 -0500

    Permit passing optional requests when adding a job.

--- test/qbwc/integration/request_generation_test.rb
+++ test/qbwc/integration/request_generation_test.rb
@@ -1,3 +1,4 @@
+$:<< File.expand_path(File.dirname(__FILE__) + '/../..')  # (for wash_out_helper.rb)
 require 'test_helper.rb'
 
 class RequestGenerationTest < ActionDispatch::IntegrationTest
@@ -21,6 +22,20 @@ class RequestGenerationTest < ActionDispatch::IntegrationTest
   test "worker with nothing" do
     QBWC.add_job(:integration_test, true, '', QBWC::Worker)
     session = QBWC::Session.new('foo', '')
+    assert_nil session.next
+  end
+
+  class NilRequestWorker < QBWC::Worker
+    def requests
+      nil
+    end
+  end
+
+  test "worker with nil" do
+    QBWC.add_job(:integration_test, true, '', NilRequestWorker)
+    session = QBWC::Session.new('foo', '')
+    assert_nil session.next
+    simulate_response(session)
     assert_nil session.next
   end
 
@@ -113,6 +128,101 @@ class RequestGenerationTest < ActionDispatch::IntegrationTest
     assert_not_nil session.next
     simulate_response(session)
     assert_nil session.next
+  end
+
+  class RequestsArgumentIgnoredByRequestWorker < QBWC::Worker
+    def requests
+      {:foo => 'bar'}
+    end
+  end
+
+  test "requests argument ignored by request worker when requests is non-nil" do
+    QBWC.add_job(:integration_test, true, '', RequestsArgumentIgnoredByRequestWorker, QBWC_CUSTOMER_ADD_RQ)
+    session = QBWC::Session.new('foo', '')
+    request = session.next
+    assert_not_nil request
+    assert_match /Foo.bar.\/Foo/, request.request
+    simulate_response(session)
+    assert_nil session.next
+
+    assert_equal [{:foo => 'bar'}], QBWC::ActiveRecord::Job::QbwcJob.first[:requests]
+  end
+
+  class RequestsArgumentOverridesRequestWorker < QBWC::Worker
+    def requests
+      nil
+    end
+  end
+
+  test "requests argument overrides request worker when requests is nil" do
+    QBWC.add_job(:integration_test, true, '', RequestsArgumentOverridesRequestWorker, QBWC_CUSTOMER_ADD_RQ)
+    session = QBWC::Session.new('foo', '')
+    request = session.next
+    assert_not_nil request
+    assert_match /Name.#{QBWC_USERNAME}.\/Name/, request.request
+    simulate_response(session)
+    assert_nil session.next
+
+    assert_equal [QBWC_CUSTOMER_ADD_RQ], QBWC::ActiveRecord::Job::QbwcJob.first[:requests]
+  end
+
+  class SimulatedUserModel
+    attr_accessor :name
+  end
+
+  class RequestsArgumentEstablishesRequestEarlyWorker < QBWC::Worker
+    def requests
+      nil
+    end
+  end
+
+  test "requests argument establishes request early" do
+    usr = SimulatedUserModel.new
+    usr.name = QBWC_USERNAME
+
+    QBWC.add_job(:integration_test, true, '', RequestsArgumentEstablishesRequestEarlyWorker, {:name => usr.name})
+    usr.name = 'bleech'
+
+    session = QBWC::Session.new('foo', '')
+    request = session.next
+    assert_match /Name.#{QBWC_USERNAME}.\/Name/, request.request
+
+    assert_equal [{:name => QBWC_USERNAME}], QBWC::ActiveRecord::Job::QbwcJob.first[:requests]
+  end
+
+  class RequestsArgumentReturnsMultipleRequestsWorker < QBWC::Worker
+    def requests
+      nil
+    end
+  end
+
+  test "requests argument returns multiple requests" do
+    usr1 = SimulatedUserModel.new
+    usr1.name = QBWC_USERNAME
+
+    usr2 = SimulatedUserModel.new
+    usr2.name = 'usr2 name'
+
+    multiple_requests = [
+      {:name => usr1.name},
+      {:name => usr2.name}
+    ]
+    QBWC.add_job(:integration_test, true, '', RequestsArgumentEstablishesRequestEarlyWorker, multiple_requests)
+    usr1.name = 'bleech'
+    usr2.name = 'bleech'
+
+    session = QBWC::Session.new('foo', '')
+    request1 = session.next
+    assert_match /Name.#{QBWC_USERNAME}.\/Name/, request1.request
+    simulate_response(session)
+
+    request2 = session.next
+    assert_match /Name.usr2 name.\/Name/, request2.request
+    simulate_response(session)
+
+    assert_nil session.next
+
+    assert_equal [{:name => QBWC_USERNAME}, {:name => 'usr2 name'}], QBWC::ActiveRecord::Job::QbwcJob.first[:requests]
   end
 
 end
