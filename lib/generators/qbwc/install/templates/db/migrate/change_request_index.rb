12127ae08e7200eea85db66d09b0e23cfa1a98b5
Stephen Sullivan <steve.t.sullivan@gmail.com>
2017-07-17 15:12:54 -0400

    Update change_request_index.rb

--- lib/generators/qbwc/install/templates/db/migrate/change_request_index.rb
+++ lib/generators/qbwc/install/templates/db/migrate/change_request_index.rb
@@ -1,4 +1,4 @@
-class ChangeRequestIndex < ActiveRecord::Migration
+class ChangeRequestIndex < ActiveRecord::Migration[5.1]
   def change
     change_column :qbwc_jobs, :request_index, :text, null: true, default: nil
   end
