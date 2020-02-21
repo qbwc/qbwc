dbe3c978921c50de929c7ef911b12e39d01dcdf7
Rina-Y <50926620+Rina-Y@users.noreply.github.com>
2020-02-20 18:11:40 -0900

    saved requestID in session (#132)

--- lib/qbwc/session.rb
+++ lib/qbwc/session.rb
@@ -61,8 +61,9 @@ class QBWC::Session
     request = self.next_request
     if request && self.iterator_id.present?
       request = request.to_hash
+      requestID = request.values.first["xml_attributes"]["requestID"]
       request.delete('xml_attributes')
-      request.values.first['xml_attributes'] = {'iterator' => 'Continue', 'iteratorID' => self.iterator_id}
+      request.values.first['xml_attributes'] = {'iterator' => 'Continue', 'iteratorID' => self.iterator_id, 'requestID' => requestID}
       request = QBWC::Request.new(request)
     end 
     request
